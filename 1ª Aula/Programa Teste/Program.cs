8f327a851dedf76d3f70042c6022ea93126c823c
unknown <renanmastroto4@gmail.com>
2026-02-25 22:12:52 -0300

    Finalizado 1ª aula

---
+++ 1ª Aula/Programa Teste/Program.cs
@@ -0,0 +1,17 @@
+﻿bool SubstringTest(string texto){
+	// CT1 
+	string s1 = texto.Substring(0); // Deve retornar “Fatec”
+	if (s1 != texto){
+		Console.WriteLine("Não passou no CT1.");
+		return false;
+	}
+	// CT2
+	string s2 = texto.Substring(4); // Deve retornar “c”
+	if (s2 != texto[4].ToString()){
+		Console.WriteLine("Não passou no CT2.");
+		return false;
+	}
+	return true;
+}
+
+System.Console.WriteLine(SubstringTest(Console.ReadLine()));
